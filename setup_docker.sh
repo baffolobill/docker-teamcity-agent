8fe0527c59a543295f8390771027de84a916468a
baffolobill <baffolobill@yandex.ru>
2017-03-20 18:26:57 +0300

    hope it fixed permission problem

--- setup_docker.sh
+++ setup_docker.sh
@@ -4,4 +4,6 @@ set -e
 wget -P /tmp https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz
 tar -xzf /tmp/docker-latest.tgz -C /usr/bin/ && rm /tmp/docker-latest.tgz
 chmod +x /usr/bin/docker
+groupadd docker
+usermod -aG docker $USER
 echo Successfuly installed docker client
