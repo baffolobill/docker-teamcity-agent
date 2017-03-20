09af69b73f9ca4046d05ac39d81f9d44d4fc9f29
baffolobill <baffolobill@yandex.ru>
2017-03-20 17:55:30 +0300

    fixed extract path

--- setup_docker.sh
+++ setup_docker.sh
@@ -2,6 +2,6 @@
 set -e
 
 wget -P /tmp https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz
-tar -xzf /tmp/docker-latest.tgz -C / && rm /tmp/docker-latest.tgz
-chmod +x /usr/local/bin/docker
+tar -xzf /tmp/docker-latest.tgz -C /usr/bin/ && rm /tmp/docker-latest.tgz
+chmod +x /usr/bin/docker
 echo Successfuly installed docker client
